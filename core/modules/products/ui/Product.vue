5b9cca86d0529078dcdc4ee3af49a01dac0abd43
Yo.Pitchayakorn <98311957+arteyo14@users.noreply.github.com>
2024-11-04 16:38:04 +0700

    เพิ่ม หน้า product

---
+++ core/modules/products/ui/Product.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <h1>prduct page</h1>
+  </div>
+</template>
+<style lang="scss" scoped></style>
---
+++ pages/product/[id]/index.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+import { Product } from "~/core/modules/products/ui";
+</script>
+<template>
+  <Product />
+</template>
+<style lang="scss" scoped></style>
