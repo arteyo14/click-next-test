a96c483384967cd738994ce4135e9c62f827edd8
Yo.Pitchayakorn <98311957+arteyo14@users.noreply.github.com>
2024-11-04 09:27:18 +0700

    clear detail

--- server/api/products/index.get.ts
+++ server/api/products/index.get.ts
@@ -5,10 +5,6 @@ const prisma = new PrismaClient();
 
 export default defineEventHandler(async (event) => {
   try {
-    // ตรวจสอบการเชื่อมต่อกับ Prisma
-    await prisma.$connect();
-
-    // ดึงข้อมูล products จากฐานข้อมูล
     const products = await prisma.products.findMany();
 
     // ส่งสถานะและข้อมูลตอบกลับ
@@ -20,18 +16,11 @@ export default defineEventHandler(async (event) => {
       data: products,
     };
   } catch (error) {
-    // พิมพ์ error เพื่อความสะดวกในการ debug
-    console.error("Error fetching products:", error);
-
-    // ตั้งค่า HTTP status code สำหรับการตอบกลับ
     setResponseStatus(event, HttpStatusCode.INTERNAL_SERVER_ERROR);
     return {
       status: false,
       code: HttpStatusCode.INTERNAL_SERVER_ERROR,
       error: { message: "Internal Server Error" },
     };
-  } finally {
-    // ปิดการเชื่อมต่อ Prisma เพื่อประหยัดทรัพยากร
-    await prisma.$disconnect();
   }
 });
